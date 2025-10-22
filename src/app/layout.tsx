e635441507a856c0126ac17e2d449449259b84a6
Prince Ongala <Princeongala99@gmail.com>
2025-10-22 11:00:36 +0000

    fais le

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,14 +68,12 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.ico' },
-      { url: '/images/itss-logo.png', sizes: '16x16', type: 'image/png' },
-      { url: '/images/itss-logo.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/images/itss-logo.png', type: 'image/png', sizes: '32x32' },
+      { url: '/images/itss-logo.png', type: 'image/png', sizes: '16x16' },
     ],
-    shortcut: ['/images/itss-logo.png'],
     apple: [
-      { url: '/images/itss-logo.png' },
-      { url: '/images/itss-logo.png', sizes: '180x180', type: 'image/png' },
+      { url: '/images/itss-logo.png', sizes: '180x180' },
     ],
   },
 };
