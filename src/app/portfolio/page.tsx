b516e05cb0f16b5820dd9d01fc171c3d1c9e2d81
Prince Ongala <Princeongala99@gmail.com>
2025-10-26 07:52:18 +0000

    cette image doit etre maintenant un peu plus visible

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -25,7 +25,7 @@ export default function PortfolioPage() {
             src={heroImage.imageUrl}
             alt={heroImage.description}
             fill
-            className="object-cover opacity-20"
+            className="object-cover opacity-30"
             data-ai-hint={heroImage.imageHint}
           />
         )}
