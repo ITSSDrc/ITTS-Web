4791fe4635a7fb7e05aef1f75def3fee230060a3
Prince Ongala <Princeongala99@gmail.com>
2025-10-29 16:16:47 +0000

    enleve SiMicrosoftazure

--- src/app/technologies/page.tsx
+++ src/app/technologies/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { ConnectionMeshAnimation } from '@/components/connection-mesh-animation';
 import {
-    SiNextdotjs, SiReact, SiTailwindcss, SiVercel, SiNodedotjs, SiPython, SiGo, SiFlutter, SiPostgresql, SiMysql, SiMongodb, SiRedis, SiDocker, SiKubernetes, SiAmazon, SiGooglecloud, SiMicrosoftazure, SiCisco, SiJunipernetworks, SiPaloaltonetworks, SiFortinet
+    SiNextdotjs, SiReact, SiTailwindcss, SiVercel, SiNodedotjs, SiPython, SiGo, SiFlutter, SiPostgresql, SiMysql, SiMongodb, SiRedis, SiDocker, SiKubernetes, SiAmazon, SiGooglecloud, SiCisco, SiJunipernetworks, SiPaloaltonetworks, SiFortinet
 } from '@icons-pack/react-simple-icons';
 
 export const metadata: Metadata = {
@@ -37,7 +37,6 @@ const technologies = {
     cloud: [
         { name: 'AWS', icon: <SiAmazon size={40} /> },
         { name: 'Google Cloud', icon: <SiGooglecloud size={40} /> },
-        { name: 'Azure', icon: <SiMicrosoftazure size={40} /> },
     ],
     network: [
         { name: 'Cisco', icon: <SiCisco size={40} /> },
